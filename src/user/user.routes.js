15c3fd65f0d4b3a475f66a7462f2aae87fe0fe1a
Josue Garcia <jgarcia-2023324@kinal.edu>
2025-02-03 17:22:58 -0600

    add: updatePassword

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import { deleteUserValidator, getUserByIdValidator } from "../middlewares/check-validator.js";
-import { getUserById, getUsers, deleteUser } from "./user.controller.js";
+import { getUserById, getUsers, deleteUser , updatePassword} from "./user.controller.js";
 const router = Router()
 
 router.get("/findUser/:uid", getUserByIdValidator, getUserById)
@@ -8,5 +8,7 @@ router.get("/findUser/:uid", getUserByIdValidator, getUserById)
 router.get("/", getUsers)
 
 router.delete("/deleteUser/:uid", deleteUserValidator, deleteUser)
+
+router.patch("/updatePassword/:uid", updatePassword)
 
 export default router
